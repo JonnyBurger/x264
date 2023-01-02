dcc79348c715eb0cdb8b9bc65de40d897d7130ce
Jonny Burger <jonathanburger11@gmail.com>
2023-01-02 16:32:37 +0100

    dangerous - implement send for encoder

--- src/encoder.rs
+++ src/encoder.rs
@@ -1,12 +1,14 @@
-use {Data, Encoding, Error, Image, Picture, Result, Setup};
 use core::{mem, ptr};
 use x264::*;
+use {Data, Encoding, Error, Image, Picture, Result, Setup};
 
 /// Encodes video.
 pub struct Encoder {
     raw: *mut x264_t,
     params: x264_param_t,
 }
+
+unsafe impl Send for Encoder {}
 
 impl Encoder {
     /// Creates a new builder with default options.
@@ -29,9 +31,7 @@ impl Encoder {
     ///
     /// Panics if there is a mismatch between the image and the encoder
     /// regarding width, height or colorspace.
-    pub fn encode(&mut self, pts: i64, image: Image)
-        -> Result<(Data, Picture)>
-    {
+    pub fn encode(&mut self, pts: i64, image: Image) -> Result<(Data, Picture)> {
         assert_eq!(image.width(), self.width());
         assert_eq!(image.height(), self.height());
         assert_eq!(image.encoding(), self.encoding());
@@ -44,9 +44,7 @@ impl Encoder {
     ///
     /// The caller must ensure that the width, height *and* colorspace
     /// of the image are the same as that of the encoder.
-    pub unsafe fn encode_unchecked(&mut self, pts: i64, image: Image)
-        -> Result<(Data, Picture)>
-    {
+    pub unsafe fn encode_unchecked(&mut self, pts: i64, image: Image) -> Result<(Data, Picture)> {
         let image = image.raw();
 
         let mut picture = mem::uninitialized();
@@ -58,13 +56,7 @@ impl Encoder {
         let mut stuff = mem::uninitialized();
         let mut raw = mem::uninitialized();
 
-        let err = x264_encoder_encode(
-            self.raw,
-            &mut stuff,
-            &mut len,
-            &mut picture,
-            &mut raw
-        );
+        let err = x264_encoder_encode(self.raw, &mut stuff, &mut len, &mut picture, &mut raw);
 
         if err < 0 {
             Err(Error)
@@ -80,13 +72,7 @@ impl Encoder {
         let mut len = 0;
         let mut stuff = unsafe { mem::uninitialized() };
 
-        let err = unsafe {
-            x264_encoder_headers(
-                self.raw,
-                &mut stuff,
-                &mut len
-            )
-        };
+        let err = unsafe { x264_encoder_headers(self.raw, &mut stuff, &mut len) };
 
         if err < 0 {
             Err(Error)
@@ -114,9 +100,13 @@ impl Encoder {
     }
 
     /// The width required of any input images.
-    pub fn width(&self) -> i32 { self.params.i_width }
+    pub fn width(&self) -> i32 {
+        self.params.i_width
+    }
     /// The height required of any input images.
-    pub fn height(&self) -> i32 { self.params.i_height }
+    pub fn height(&self) -> i32 {
+        self.params.i_height
+    }
     /// The encoding required of any input images.
     pub fn encoding(&self) -> Encoding {
         unsafe { Encoding::from_raw(self.params.i_csp) }
@@ -125,7 +115,9 @@ impl Encoder {
 
 impl Drop for Encoder {
     fn drop(&mut self) {
-        unsafe { x264_encoder_close(self.raw); }
+        unsafe {
+            x264_encoder_close(self.raw);
+        }
     }
 }
 
@@ -147,23 +139,18 @@ impl Flush {
         let mut stuff = unsafe { mem::uninitialized() };
         let mut raw = unsafe { mem::uninitialized() };
 
-        let err = unsafe {
-            x264_encoder_encode(
-                enc,
-                &mut stuff,
-                &mut len,
-                ptr::null_mut(),
-                &mut raw
-            )
-        };
+        let err =
+            unsafe { x264_encoder_encode(enc, &mut stuff, &mut len, ptr::null_mut(), &mut raw) };
 
         Some(if err < 0 {
             Err(Error)
         } else {
-            Ok(unsafe {(
-                Data::from_raw_parts(stuff, len as usize),
-                Picture::from_raw(raw),
-            )})
+            Ok(unsafe {
+                (
+                    Data::from_raw_parts(stuff, len as usize),
+                    Picture::from_raw(raw),
+                )
+            })
         })
     }
 }
